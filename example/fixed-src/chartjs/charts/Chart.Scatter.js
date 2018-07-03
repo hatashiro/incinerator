248502e6c714b3f8b2975d930813432ac1df2b62
Hyunje Jun <me@noraesae.net>
2018-07-04 03:56:39 +0900

    Make example simpler

--- example/fixed-src/chartjs/charts/Chart.Scatter.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = function (Chart) {
-  Chart.Scatter = function () {};
-};
--- example/fixed-src/chartjs/platforms/platform.basic.js
+++
@@ -1,7 +0,0 @@
-/**
- * Platform fallback implementation (minimal).
- * @see https://github.com/chartjs/Chart.js/pull/4591#issuecomment-319575939
- */
-module.exports = {
-  acquireContext: function () {}
-};
