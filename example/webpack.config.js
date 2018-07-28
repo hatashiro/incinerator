b1a95d3df0f9d0a3c849e2e510206f7876a1f08b
Hyunje Jun <me@noraesae.net>
2018-07-28 17:29:04 +0000

    Lengthen watch's aggregate timeout

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -4,6 +4,9 @@ let isWatching = process.argv.includes("--watch");
 let isAnalyzing = process.argv.includes("--analyze");
 
 module.exports = {
+  watchOptions: {
+    aggregateTimeout: 2000
+  },
   plugins: isAnalyzing ? [
     new BundleAnalyzerPlugin()
   ] : []
